f9e8ca26020120fececb1ac9d153fe1d21fdcd8f
crlse42 <carlosdlt42@hotmail.com>
2023-11-14 13:03:05 -0700

    Code with assistance from tutor.

---
+++ React_Portfolio/src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+        <>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </>
+    )
+}
+
+export default NavBar
